2a8b488a985aa450ae6c407c7a98cdcf95626754
unknown <toghrul@code.edu.az>
2022-08-23 18:14:32 +0400

    . .

---
+++ 2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWorkTask
+{
+    class Room
+    {
+        public readonly int _id;
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int PersonCapacity { get; set; }
+        public bool isAvailable { get; set; } = true;
+
+        public Room(int Id, string Name, double Price, int PersonCapacity, bool IsAvailable)
+        {
+            Id = _id;
+            Name = Name;
+            Price = Price;
+            PersonCapacity = PersonCapacity;
+            IsAvailable = isAvailable;
+        }
+        public Room()
+        {
+            _id++;
+            _id = _id;
+        }
+        public string ShowInfo()
+        {
+            return ("Room name: " + Name + "\nRoom price: " + Price + "\nRoom person capacity: " + PersonCapacity + "\nRoom is aviable: " + isAvailable);
+        }
+        public override string ToString()
+        {
+            return ShowInfo();
+        }
+
+
+    }
+}
---
+++ 3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HomeWorkTask
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            string value;
+            Room v = new Room();
+
+
+        }
+    }
+}
